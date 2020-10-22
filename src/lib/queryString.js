0b1422d632274cfda7fa0b89992126d020c96fd7
Carol <ommasshi@gmail.com>
2020-10-22 14:52:21 -0300

    Separação das funções por responsabilidade

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,9 +1,11 @@
+const keyValueToString = ([prop, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Only Arrays, strings and numbers are allowed here!')
+  }
+  return `${prop}=${value}`;
+}
+
 module.exports.queryString = obj =>
   Object.entries(obj)
-    .map(([prop, value]) => {
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        throw new Error('Only Arrays, strings and numbers are allowed here!')
-      }
-      return `${prop}=${value}`;
-    })
+    .map(keyValueToString)
     .join('&');
